40afb696ad725aad20c547ca13397b5a5086e31b
sulimannxx <102816218+sulimannxx@users.noreply.github.com>
2022-04-11 01:40:43 +0300

    v1

---
+++ SimplePlatformer/Assets/Coin.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent(out Player player))
+        {
+            player.AddCoin();
+            Destroy(gameObject);
+        }
+    }
+}
