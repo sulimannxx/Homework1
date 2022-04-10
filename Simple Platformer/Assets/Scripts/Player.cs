82e985678cbb54d303cc742e1cb554b3d4e26de9
sulimannxx <102816218+sulimannxx@users.noreply.github.com>
2022-04-11 01:27:03 +0300

    Initial upload v1

---
+++ Simple Platformer/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+
+public class Player : MonoBehaviour
+{
+    public bool IsDead { get; private set; }
+    public int Coins { get; private set; }
+
+    private Animator _playerAnimator;
+
+    private void Start()
+    {
+        _playerAnimator = GetComponent<Animator>();
+    }
+
+    public void SetDead()
+    {
+        IsDead = true;
+    }
+
+    public void SetAnimatorInactive()
+    {
+        _playerAnimator.enabled = false;
+    }
+
+    public void AddCoin()
+    {
+        Coins += 1;
+        Debug.Log(Coins);
+    }
+}
